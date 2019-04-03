fbbbfa51c6e2c9f9589f17743dc575107fe4d898
Britta <polished.athena@gmail.com>
2019-04-03 12:24:28 -0500

    fixed gif

--- assests/java.js
+++ assests/java.js
@@ -1,28 +1,33 @@
 
+//var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RYD0xnN1DY9rH29nIj8R8FcMC53D6Pvb&tag=&rating=PG";
+
+
 //submit button
-$("#submit").on("click", function() {
-//preform ajax get function
-var animal = $('gifInput').val();
-var queryURL = "https://api.giphy.com/v1/gifs/search?q="+animal+"&api_key=RYD0xnN1DY9rH29nIj8R8FcMC53D6Pvb&tag=&rating=PG";
-      // Perfoming an AJAX GET request to our queryURL
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      // After the data from the AJAX request comes back
-      .then(function(response) {
-
+$("#submit").on("click", function () {
+  //preform ajax get function
+  var animal = $('#gifInput').val();
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=ULFOH5F6556uy1PEseD33ankMx15dEQn&tag=&rating=PG";
+  // Perfoming an AJAX GET request to our queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After the data from the AJAX request comes back
+    .then(function (response) {
+      console.log(response);
+      for (var i = 0; i < response.data.length; i++) {
         // Saving the image_original_url property
-          var imageUrl = response.data.image_original_url;
+        var imageUrl = response.data[i].images.original.url;
 
-          // Creating and storing an image tag
-          var gifImage = $("<img>");
+        // Creating and storing an image tag
+        var gifImage = $("<img>");
 
-          // Setting src attribute to imageUrl
-          gifImage.attr("src", imageUrl);
-          gifImage.attr("alt", "giphy");
+        // Setting src attribute to imageUrl
+        gifImage.attr("src", imageUrl);
+        gifImage.attr("alt", "giphy");
 
-          // Prepending to the images div
-          $("#images").prepend(gifImage);
-        });
+        // Prepending to the images div
+        $("#images").prepend(gifImage);
+      }
     });
+});
